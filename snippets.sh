51c889a1a612c794f3299f6f0c8e2172cc699e3b
con0101 <124743273+con0101@users.noreply.github.com>
2026-04-08 22:01:44 -0400

    Delete snippets.sh

--- snippets.sh
+++
@@ -1,5 +0,0 @@
-# Run command as 'kali' from an elevated execution:
-sudo -u kali env \
-  HOME=/home/kali \
-  XDG_RUNTIME_DIR=/run/user/$(id -u kali) \
-  DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u kali)/bus \
